a1505abb7e323dcf8718a4eaed6220f96efa5747
shourya-r <rajputshourya7@gmail.com>
2025-01-14 22:06:36 +0530

    Added the AuthLayout component

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    // if logged out and authentication is required then redirect
+    // to login page
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+      // if logged in and authentication is not required then redirect
+      // to home page
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1>Loading...</h1> : { children };
+}
